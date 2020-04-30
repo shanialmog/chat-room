0545699cdbf802b8ca437ba9d7bd56f14f68a1fe
Shani Almog <killerdrum@gmail.com>
2020-04-30 23:10:40 +0300

    add text field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div>
+      <CssBaseline />
       <h1>Chat room</h1>
+      <TextField
+        id="outlined-multiline-static"
+        label="Shani"
+        multiline
+        rows={4}
+        placeholder="Text here"
+        variant="outlined"
+      />
+      <div>
+        <Button variant="contained">Send</Button>
+      </div>
     </div>
   )
 }
