0568bb9f44a882684cfeaa59f9bc3def7befa48e
Shani Almog <killerdrum@gmail.com>
2020-05-02 21:11:36 +0300

    add automatic scroll, add message sent from user to display, add enter press to send message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import io from 'socket.io-client'
 import TextField from '@material-ui/core/TextField'
 import CssBaseline from '@material-ui/core/CssBaseline'
-// import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import SendRoundedIcon from '@material-ui/icons/SendRounded'
+import moment from 'moment'
 
 function App() {
   const [message, setMessage] = useState("")
   const [response, setResponse] = useState([])
   const [socket, setSocket] = useState(null)
+
 
   useEffect(() => {
     const openSocket = io.connect()
@@ -19,30 +20,49 @@ function App() {
     });
   }, []);
 
+  const messagesEndRef = useRef(null)
+
+  useEffect(() => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+  }, [response]);
+
   const handleChange = (event) => {
     setMessage(event.target.value)
   }
 
-  const sendMsg = () => {
+  const sendMsg = (event) => {
     socket.emit("msg", { "message": message, "user": "shani" })
+    const t = Date.now()
+    const time = Math.floor(t / 1000)
+    setResponse((prevstate) => { return ([...prevstate, { "message": message, "user": "Shani", "timestamp": time }]) })
   }
-  console.log("response", response)
+
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      sendMsg()
+    }
+  }
+
 
   return (
     <div className="page-cont">
       <CssBaseline />
-      <h1>Chat room</h1>
-      <div 
+      <h1
+        className="header"
+      >Chat room</h1>
+      <div
         className="chat-msg-cont"
       >
         {response.map((res) => {
+          const t = moment.unix(res.timestamp)
           return (
             <div
               className="chat-msg"
-              key={res.timestamp}
+              key={res.timestamp + res.message}
             >
               <TextField
-                label={res.timestamp + res.user}
+                label={`${res.user} ${t.fromNow()}`}
                 value={res.message}
                 multiline
                 rowsMax={4}
@@ -53,6 +73,7 @@ function App() {
             </div>
           )
         })}
+        <div ref={messagesEndRef}></div>
       </div>
       <div className="user-msgbox">
         <div className="user-msg">
@@ -65,9 +86,11 @@ function App() {
             variant="outlined"
             fullWidth
             onChange={handleChange}
+            onKeyDown={handleKeyDown}
           />
           <div className="send-btn">
             <IconButton
+              type="submit"
               onClick={sendMsg}
               edge="end"
               color="primary"
