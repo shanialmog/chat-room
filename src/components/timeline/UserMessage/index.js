27b34664f51d8037818c3faf536f0aaf8d452c97
Shani Almog <killerdrum@gmail.com>
2020-05-20 21:42:46 +0300

    add markdown to timeline message box and change css

--- src/components/timeline/UserMessage/index.js
+++ src/components/timeline/UserMessage/index.js
@@ -1,18 +1,60 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField'
+// import TextField from '@material-ui/core/TextField'
+import ReactMarkdown from 'react-markdown'
+import InputLabel from "@material-ui/core/InputLabel"
+import NotchedOutline from "@material-ui/core/OutlinedInput/NotchedOutline"
 import moment from 'moment'
 
 export default ({ name, message, timestamp }) => {
     const t = moment.unix(timestamp)
+    const allowedTypes = ['image', 'strong ', 'emphasis ', 'definition ']
+
     return (
-        <TextField
-            label={`${name} ${t.fromNow()}`}
-            value={message}
-            multiline
-            rowsMax={4}
-            placeholder="Text here"
-            variant="outlined"
-            fullWidth
-        />
+        <div style={{ position: "relative", marginTop: "8px" }}>
+            <InputLabel
+                variant="outlined"
+                // className={classes.inputLabel}
+                shrink
+            >
+                {`${name} ${t.fromNow()}`}
+            </InputLabel>
+            {/* <div> */}
+                <div style={{padding: "2px 14px"}}>
+                    <ReactMarkdown source={message} />
+                    <NotchedOutline colorSecondary notched labelWidth="25" />
+                    {/* <NotchedOutline notched /> */}
+                </div>
+            {/* </div> */}
+        </div>
+        // <div>
+        /* <div style={{
+            // ...titleStyle,
+            transform: 'translate(-43px, -11px) scale(0.75)',
+            fontSize: '17px',
+            color: 'rgba(0, 0, 0, 0.54)',
+            position: 'absolute',
+        }}
+        >
+            {`${name} ${t.fromNow()}`}
+        </div>
+        <div
+            // className="flex-row"
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.23)',
+                padding: '5px 14px',
+                borderRadius: '4px',
+            }}
+        >
+            <ReactMarkdown source={message} />
+        </div> */
+        /* </div> */
+
+
+        // <TextField
+        //     label={`${name} ${t.fromNow()}`}
+        //     value={message}
+        //     variant="outlined"
+        //     fullWidth
+        // />
     )
 }
